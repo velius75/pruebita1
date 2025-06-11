3d3e91da3bd4a4f377e486d2408edeeb219348c9
Edward Ruesta <ruestaarroyo@gmail.com>
2025-06-10 19:02:32 -0500

    cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,48 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /*
+    *
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hola mundo 2025!");
+        //System.out.printf("Hola mundo 2025!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        // primitivo: int, boolean
+        // Wrapper : Integer, Boolean, String
+
+        Boolean variable = null;
+
+        int a = 1;//sasasasas
+
+        int b = 2;
+
+        int c = a-=b;
+
+        Long x = null;
+
+        System.out.println(x);
+
+
+//        if (b == a) {
+//            System.out.println("hola");
+//        } else {
+//            System.out.println("chao");
+//        }
+//
+//        System.out.println(b==a?"chao":"hola");//TERNARIA
+
+
+
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//
+//            //comentario aqui
+//            System.out.println("i = " + i);  //comentario aqui
+//            //comentario aqui
+//        }
     }
 }
